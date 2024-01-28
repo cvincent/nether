1e0d19e120dcd40bd8bc6218e81df8d83948cd91
Chris Vincent <chris.vincent@hey.com>
2024-01-28 08:10:32 -0600

    Automatically generate and email invoices

--- apps/documents/hm.nix
+++ apps/documents/hm.nix
@@ -3,5 +3,7 @@
 {
   home.packages = with pkgs; [
     texlive.combined.scheme-medium # For generating PDFs from LaTeX
+
+    (pkgs.writeShellScriptBin "generate-invoice" (builtins.readFile ./generate-invoice.bash))
   ];
 }
