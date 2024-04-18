1cde6010a52ea92d372779e5b12d376a1ae2597e
Chris Vincent <chris.vincent@hey.com>
2024-04-18 14:54:18 -0500

    Nordzy icons

--- stylix/hm.nix
+++ stylix/hm.nix
@@ -1,8 +1,21 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
     inputs.stylix.homeManagerModules.stylix
     ./common.nix
   ];
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Nordzy-dark";
+      package = pkgs.nordzy-icon-theme;
+      # name = "WhiteSur";
+      # package = pkgs.whitesur-icon-theme.override {
+      #   # themeVariants = ["nord"];
+      #   alternativeIcons = true;
+      # };
+    };
+  };
 }
