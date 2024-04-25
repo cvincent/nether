13d1f105f529df9d623436cf2067fd523b1c7ee2
Chris Vincent <chris.vincent@hey.com>
2024-04-25 14:51:20 -0500

    Add vim-dadbod and vim-dadbod-ui

--- apps/nvim/configs/init.lua
+++ apps/nvim/configs/init.lua
@@ -211,6 +211,23 @@ require("lazy").setup({
     },
     opts = {}
   },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 })
 
 -- Vim in the browser
