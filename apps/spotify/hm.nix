537dd2b5c6b1d820605dbf5dc60758b85267aa9a
Chris Vincent <chris.vincent@hey.com>
2024-04-29 09:14:30 -0500

    Basic Nord theme for ncspot

--- apps/spotify/hm.nix
+++ apps/spotify/hm.nix
@@ -4,5 +4,26 @@
   programs.ncspot = {
     enable = true;
     package = nixpkgs-unstable.ncspot;
+    settings = {
+      theme = {
+        background = "default";
+        primary = "#D8DEE9";
+        secondary = "#4C566A";
+        title = "#EBCB8B";
+        playing = "#2E3440";
+        playing_selected = "#FF6060";
+        playing_bg = "#88C0D0";
+        highlight = "#88C0D0";
+        highlight_bg = "#4C566A";
+        error = "#ECEFF4";
+        error_bg = "#BF616A";
+        statusbar = "#2E3440";
+        statusbar_progress = "#A3BE8C";
+        statusbar_bg = "#8FBCBB";
+        cmdline = "#D8DEE9";
+        cmdline_bg = "#2E3440";
+        search_match = "#B48EAD";
+      };
+    };
   };
 }
