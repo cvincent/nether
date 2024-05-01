11ffcff22ea1c6646e640b798545c6657277a199
Chris Vincent <chris.vincent@hey.com>
2024-05-01 16:40:15 -0500

    The Lazy spec documented by Neorg is incorrect lol

--- apps/nvim/configs/init.lua
+++ apps/nvim/configs/init.lua
@@ -211,7 +211,6 @@ require("lazy").setup({
     dependencies = { "luarocks.nvim" },
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
-    config = true,
   },
 
   {
