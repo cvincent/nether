996f6259c631d8a2974765c1774f9ec15ed801ac
Chris Vincent <chris.vincent@hey.com>
2024-11-11 21:28:38 -0600

    Very cool text-to-diagram system

--- apps/documents/hm.nix
+++ apps/documents/hm.nix
@@ -3,6 +3,7 @@
 {
   home.packages = with pkgs; [
     texlive.combined.scheme-medium # For generating PDFs from LaTeX
+    d2 # Text to diagrams
 
     (pkgs.writeShellScriptBin "generate-invoice" (builtins.readFile ./generate-invoice.bash))
   ];
