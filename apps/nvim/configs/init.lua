eb4caf176e4e2c92496657413de527cffcc10d49
Chris Vincent <chris.vincent@hey.com>
2024-11-11 22:05:01 -0600

    Install luarocks in our system rather than luarocks.nvim
    Per most current installation instructions for Lazy

--- apps/nvim/configs/init.lua
+++ apps/nvim/configs/init.lua
@@ -199,13 +199,6 @@ require("lazy").setup({
   -- Other --
   -----------
 
-  -- Needed by Neorg
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
-  },
-
   {
     "nvim-neorg/neorg",
     dependencies = { "luarocks.nvim" },
--- apps/nvim/hm.nix
+++ apps/nvim/hm.nix
@@ -4,7 +4,7 @@
   # Treesitter wants a C compiler
   # Neorg wants LuaRocks which wants a non-embedded Lua install, specifically
   # 5.1. It also wants make.
-  home.packages = with pkgs; [ gcc lua5_1 gnumake ];
+  home.packages = with pkgs; [ gcc lua5_1 lua51Packages.luarocks gnumake ];
   home.file."./.config/nvim".source = utils.directSymlink "apps/nvim/configs";
   home.sessionVariables.EDITOR = "nvim";
 }
