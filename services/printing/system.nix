d759ad7ab73c4d1fac13c3552c2391a10e56fc28
Chris Vincent <chris.vincent@hey.com>
2024-11-11 21:27:31 -0600

    Make printing work

--- services/printing/system.nix
+++ services/printing/system.nix
@@ -3,6 +3,23 @@
 {
   services.printing.enable = true;
 
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "OfficeJet-4650";
+        location = "Home";
+        deviceUri = "hp:/net/OfficeJet_4650_series?ip=192.168.1.124";
+        model = "drv:///sample.drv/generic.ppd";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "OfficeJet-4650";
+  };
+
+  services.printing.drivers = [ pkgs.hplip ];
+
   # Supports pretty much any "Apple compatible" printer, without drivers
   services.avahi = {
     enable = true;
