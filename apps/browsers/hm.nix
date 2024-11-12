df823efcf6fc28e2378f669579014dc07d6b6110
Chris Vincent <chris.vincent@hey.com>
2024-11-11 18:59:13 -0600

    Update browsers, enable HWA video encoding

--- apps/browsers/hm.nix
+++ apps/browsers/hm.nix
@@ -1,9 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, nixpkgs-unstable, ... }:
 
 {
   home.packages = with pkgs; [
-    chromium
-    brave
+    (nixpkgs-unstable.chromium.override {
+      commandLineArgs = [
+        "--enable-features=VaapiVideoEncoder"
+        "--ignore-gpu-blocklist"
+        "--enable-zero-copy"
+      ];
+    })
+    (nixpkgs-unstable.brave.override {
+      commandLineArgs = [
+        "--enable-features=VaapiVideoEncoder"
+        "--ignore-gpu-blocklist"
+        "--enable-zero-copy"
+      ];
+    })
     firefox
   ];
 }
