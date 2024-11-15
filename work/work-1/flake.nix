c9f7d21a25b5dc2bedaf1bd11dc3222a433ecfb9
Chris Vincent <chris.vincent@hey.com>
2024-11-15 14:20:00 -0600

    Include Yarn I guess, but didn't help me run ESLint

--- work/work-1/flake.nix
+++ work/work-1/flake.nix
@@ -10,6 +10,8 @@
     our-elixir.url = "github:nixos/nixpkgs/90e85bc7c1a6fc0760a94ace129d3a1c61c3d035";
     # NodeJS 18.14.1
     our-nodejs.url = "github:nixos/nixpkgs/06365ba4549654b7ce58c33365c1282800e83a9e";
+    # Yarn 1.22.22
+    our-yarn.url = "github:nixos/nixpkgs/4ae2e647537bcdbb82265469442713d066675275";
     # PostgreSQL 14.10
     our-postgresql.url = "github:nixos/nixpkgs/f5c27c6136db4d76c30e533c20517df6864c46ee";
   };
@@ -46,6 +48,7 @@
           inputs.our-elixir.legacyPackages.${system}.elixir_1_15
           our-elixir-ls
           inputs.our-nodejs.legacyPackages.${system}.nodejs
+          inputs.our-yarn.legacyPackages.${system}.yarn
           inputs.our-postgresql.legacyPackages.${system}.postgresql_14
           inotify-tools
           # We run redis-stack-server using an AppImage because nobody has built
