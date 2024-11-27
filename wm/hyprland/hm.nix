54ed2fa39a802187f32785044420584bc7e78a77
Chris Vincent <chris.vincent@hey.com>
2024-11-26 21:28:53 -0600

    reload-scratchpads function; format

--- wm/hyprland/hm.nix
+++ wm/hyprland/hm.nix
@@ -1,25 +1,32 @@
-{ config, pkgs, utils, nixpkgs-unstable, ... }:
-
 {
-  nixpkgs.overlays = [(self: super: {
-    # Bring in latest which is patched to actually launch
-    hyprpicker-latest = super.hyprpicker.overrideAttrs (old: {
-      src = super.fetchFromGitHub {
-        owner = "hyprwm";
-        repo = "hyprpicker";
-        rev = "b6130e3901ed5c6d423f168705929e555608d870";
-        sha256 = "sha256-x+6yy526dR75HBmTJvbrzN+sXINVL26yN5TY75Dgpwk=";
-      };
-    });
-  })];
+  pkgs,
+  utils,
+  ...
+}:
+let
+  scratchpads = import ./scratchpads.nix;
+  scratchpads-classes = builtins.concatStringsSep "," (
+    map ({ class, ... }: "\"${class}\"") scratchpads
+  );
+in
+{
+  nixpkgs.overlays = [
+    (self: super: {
+      # Bring in latest which is patched to actually launch
+      hyprpicker-latest = super.hyprpicker.overrideAttrs (old: {
+        src = super.fetchFromGitHub {
+          owner = "hyprwm";
+          repo = "hyprpicker";
+          rev = "b6130e3901ed5c6d423f168705929e555608d870";
+          sha256 = "sha256-x+6yy526dR75HBmTJvbrzN+sXINVL26yN5TY75Dgpwk=";
+        };
+      });
+    })
+  ];
 
   imports = [ ../wayland/hm.nix ];
 
-  home = let
-    scratchpads = import ./scratchpads.nix;
-    scratchpads-classes = builtins.concatStringsSep "," (map ({ class, ... }: "\"${class}\"") scratchpads);
-  in
-  {
+  home = {
     packages = with pkgs; [
       pyprland
       hyprpicker-latest
@@ -50,27 +57,48 @@
       "./.config/hypr/pyprland.toml".text = builtins.concatStringsSep "\n" (
         [
           ''
-          [pyprland]
-          plugins = ["scratchpads", "magnify"]
+            [pyprland]
+            plugins = ["scratchpads", "magnify"]
           ''
-        ] ++
-        map ({ class, command, ... }: ''
-          [scratchpads.${class}]
-          command = "${command}"
-          animation = "fromTop"
-          unfocus = "hide"
-          margin = 50
-        '') scratchpads
+        ]
+        ++ map (
+          { class, command, ... }:
+          ''
+            [scratchpads.${class}]
+            command = "${command}"
+            animation = "fromTop"
+            unfocus = "hide"
+            margin = 50
+          ''
+        ) scratchpads
       );
 
       "./.config/hypr/scratchpads.conf".text = builtins.concatStringsSep "\n" (
-        map ({ class, binding, size ? "60% 60%", ... }: ''
-          bind = ${binding}, exec, switch-scratchpad ${class}
-          windowrule = float, ^(${class})$
-          windowrule = size ${size}, ^(${class})$
-          windowrule = move 30% -200%, ^(${class})$
-        '') scratchpads
+        map (
+          {
+            class,
+            binding,
+            size ? "60% 60%",
+            ...
+          }:
+          ''
+            bind = ${binding}, exec, switch-scratchpad ${class}
+            windowrule = float, ^(${class})$
+            windowrule = size ${size}, ^(${class})$
+            windowrule = move 30% -200%, ^(${class})$
+          ''
+        ) scratchpads
       );
     };
+  };
+
+  programs.fish.functions = {
+    reload-scratchpads = builtins.concatStringsSep "\n" (
+      (map ({ class, ... }: "pkill --full ${class}") scratchpads)
+      ++ [
+        "pkill --full pyprland"
+        "pypr & disown"
+      ]
+    );
   };
 }
