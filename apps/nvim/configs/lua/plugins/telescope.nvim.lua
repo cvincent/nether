0102e6e39cea4e4b30b3acd6746a18be781caf90
Chris Vincent <chris.vincent@hey.com>
2024-12-14 01:29:50 -0600

    Format

--- apps/nvim/configs/lua/plugins/telescope.nvim.lua
+++ apps/nvim/configs/lua/plugins/telescope.nvim.lua
@@ -12,8 +12,8 @@ return {
         },
         winblend = 28,
         layout_config = {
-          width = 0.6;
-          height = 0.6;
+          width = 0.6,
+          height = 0.6,
         }
       },
       pickers = {
