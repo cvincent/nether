bd4655b55ccd1b041fcff88067da21cbb31b8b13
Chris Vincent <chris.vincent@hey.com>
2024-12-14 02:02:13 -0600

    Only set up format autocmd if formatting is supported

--- apps/nvim/configs/lua/plugins/nvim-lspconfig.lua
+++ apps/nvim/configs/lua/plugins/nvim-lspconfig.lua
@@ -78,12 +78,17 @@ return {
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("lsp", { clear = true }),
       callback = function(args)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = args.buf,
-          callback = function()
-            vim.lsp.buf.format { async = false, id = args.data.client_id }
-          end,
-        })
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client then return end
+
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format { async = false, id = args.data.client_id }
+            end,
+          })
+        end
       end
     })
   end
