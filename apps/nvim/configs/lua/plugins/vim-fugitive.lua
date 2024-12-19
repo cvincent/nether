39165fd13b14042a9d38f70ba32de6d212501520
Chris Vincent <chris.vincent@hey.com>
2024-12-18 22:52:58 -0600

    Floating vim-fugitive!

--- apps/nvim/configs/lua/plugins/vim-fugitive.lua
+++ apps/nvim/configs/lua/plugins/vim-fugitive.lua
@@ -17,10 +17,31 @@ return {
       vim.api.nvim_command("Git push origin " .. branch)
     end
 
+    local function floating_summary()
+      local ui = vim.api.nvim_list_uis()[1]
+
+      local summary = vim.fn.bufadd(".git/index")
+
+      vim.api.nvim_open_win(summary, true, {
+        relative = "editor",
+        row = ui.height - (ui.height * 0.33),
+        col = 0,
+        width = ui.width,
+        height = math.floor(ui.height * 0.33),
+        border = { "", "―", "", "", "", "", "", "" }
+      })
+
+      vim.api.nvim_command("normal )")
+
+      -- TODO: Set some buffer-local keymaps for resizing the window vertically
+      -- while keeping it anchored to the bottom
+    end
+
     vim.keymap.set("n", "<leader>gpl", function() pull_from_current_branch() end)
     vim.keymap.set("n", "<leader>gpu", function() push_to_current_branch() end)
 
-    vim.keymap.set("n", "<leader>gs", ":Git<cr>)", { silent = true })
+    vim.keymap.set("n", "<leader>gs", floating_summary, { silent = true })
+    -- vim.keymap.set("n", "<leader>gs", ":Git<cr>)", { silent = true })
     vim.keymap.set("n", "<leader>gd", ":tab Gdiff<cr>", { silent = true })
     -- vim.keymap.set("n", "<leader>gc", ":Git checkout ")
     vim.keymap.set("n", "<leader>gb", ":Git checkout -b ")
@@ -31,7 +52,22 @@ return {
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "gitcommit",
-      command = "nmap <buffer> <leader>w :wq<cr>"
+      callback = function(ev)
+        local wins = vim.api.nvim_list_wins()
+        for i, win in ipairs(wins) do
+          if vim.api.nvim_win_get_config(win).relative == "editor" then
+            vim.api.nvim_win_set_buf(win, ev.buf)
+            vim.api.nvim_win_hide(0)
+            vim.api.nvim_set_current_win(win)
+            -- TODO: Figure out how to put ourselves into insert mode
+            -- here...we've wanted to do this for like 10 years lol
+            vim.api.nvim_command("normal i")
+            break
+          end
+        end
+
+        vim.keymap.set("n", "<leader>w", ":wq<cr>", { buffer = true })
+      end,
     })
   end
 }
