6bd5a09ffc35765d11980c8939cb7e92bdec175e
Chris Vincent <chris.vincent@hey.com>
2024-12-18 22:16:36 -0600

    We never use this, it doesn't work, and it makes our new Telescope <leader>ff mapping slow

--- apps/nvim/configs/lua/plugins/vim-eunuch.lua
+++ apps/nvim/configs/lua/plugins/vim-eunuch.lua
@@ -2,7 +2,6 @@ return {
   "tpope/vim-eunuch",
   init = function()
     vim.keymap.set("n", "<leader>fm", ":Move ")
-    vim.keymap.set("n", "<leader>ffm", ":Move <cr>=expand('%')<cr>")
     vim.keymap.set("n", "<leader>fu", ":Unlink<cr>")
   end
 }
