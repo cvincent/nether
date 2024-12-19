1ad626c102f844be207890d25c8d3f71cf6604bb
Chris Vincent <chris.vincent@hey.com>
2024-12-19 06:58:39 -0600

    Remove deprecated call, format

--- apps/nvim/configs/lua/rc/numberline.lua
+++ apps/nvim/configs/lua/rc/numberline.lua
@@ -1,26 +1,24 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.api.nvim_exec([[
+vim.api.nvim_command([[
+  function! NumberToggle()
+    if(&relativenumber == 1)
+      set number
+      set norelativenumber
+    else
+      set number
+      set relativenumber
+    endif
+  endfunc
 
-function! NumberToggle()
-  if(&relativenumber == 1)
-    set number
-    set norelativenumber
-  else
-    set number
-    set relativenumber
-  endif
-endfunc
+  nnoremap <C-N> :call NumberToggle()<CR>
 
-nnoremap <C-N> :call NumberToggle()<CR>
-
-let blacklist = ['fzf']
-au FocusLost * if index(blacklist, &ft) < 0 | set number
-au FocusLost * if index(blacklist, &ft) < 0 | set norelativenumber
-au FocusGained * if index(blacklist, &ft) < 0 | set relativenumber
-au WinLeave * if index(blacklist, &ft) < 0 | set number
-au WinLeave * if index(blacklist, &ft) < 0 | set norelativenumber
-au WinEnter * if index(blacklist, &ft) < 0 | set relativenumber
-
-]], true)
+  let blacklist = ['fzf']
+  au FocusLost * if index(blacklist, &ft) < 0 | set number
+  au FocusLost * if index(blacklist, &ft) < 0 | set norelativenumber
+  au FocusGained * if index(blacklist, &ft) < 0 | set relativenumber
+  au WinLeave * if index(blacklist, &ft) < 0 | set number
+  au WinLeave * if index(blacklist, &ft) < 0 | set norelativenumber
+  au WinEnter * if index(blacklist, &ft) < 0 | set relativenumber
+]])
