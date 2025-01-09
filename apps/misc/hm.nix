e0ea16242501f40002b5c5453fa988d045383e99
Chris Vincent <c.j.vincent@protonmail.com>
2025-01-08 20:08:04 -0600

    Emulators!

--- apps/misc/hm.nix
+++ apps/misc/hm.nix
@@ -16,6 +16,20 @@ let
       }
     )
   );
+
+  latest-shadps4 = (
+    nixpkgs-unstable.shadps4.overrideAttrs (
+      final: prev: {
+        src = nixpkgs-unstable.fetchFromGitHub {
+          owner = "shadps4-emu";
+          repo = "shadPS4";
+          rev = "a1a98966eee07e7ecf3a5e3836b5f2ecde5664b0";
+          hash = "sha256-lN+qXvf+rHlfZt7iT/De/tMvAQJpqLGOJxrv9z4tX5c=";
+          fetchSubmodules = true;
+        };
+      }
+    )
+  );
 in
 {
   home.packages = with pkgs; [
@@ -27,6 +41,8 @@ in
     nixpkgs-slack.slack
     nixpkgs-zoom.zoom-us
     xwayland-spotify
+    nixpkgs-unstable.ryujinx
+    latest-shadps4
   ];
 
   programs.zathura.enable = true;
