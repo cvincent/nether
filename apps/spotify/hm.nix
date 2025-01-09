6c61ffd29bd2ef469f3e30ff4b66b260263eaf94
Chris Vincent <c.j.vincent@protonmail.com>
2025-01-08 18:26:16 -0600

    This nc-spot theme now conflicts with Stylix

--- apps/spotify/hm.nix
+++ apps/spotify/hm.nix
@@ -1,4 +1,7 @@
-{ config, pkgs, utils, nixpkgs-unstable, ... }:
+{
+  nixpkgs-unstable,
+  ...
+}:
 
 {
   programs.ncspot = {
@@ -9,25 +12,6 @@
         "Esc" = "back";
         "Backspace" = "noop";
         "q" = "noop";
-      };
-      theme = {
-        background = "default";
-        primary = "#D8DEE9";
-        secondary = "#4C566A";
-        title = "#EBCB8B";
-        playing = "#2E3440";
-        playing_selected = "#FF6060";
-        playing_bg = "#88C0D0";
-        highlight = "#88C0D0";
-        highlight_bg = "#4C566A";
-        error = "#ECEFF4";
-        error_bg = "#BF616A";
-        statusbar = "#2E3440";
-        statusbar_progress = "#A3BE8C";
-        statusbar_bg = "#8FBCBB";
-        cmdline = "#D8DEE9";
-        cmdline_bg = "#2E3440";
-        search_match = "#B48EAD";
       };
     };
   };
