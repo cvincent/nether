4f972eba5bd1d94a7910ce8dc77a1fc9d1df6f6b
Chris Vincent <c.j.vincent@protonmail.com>
2025-01-12 21:18:10 -0600

    Sync before writing cache

--- apps/bitwarden/bitwarden-cache-vault.bash
+++ apps/bitwarden/bitwarden-cache-vault.bash
@@ -1,4 +1,5 @@
 vault_cache=~/.local/share/bitwarden/vault-cache.json
 session=$(bitwarden-ensure-session)
+bw sync --session "$session"
 bw list items --session "$session" > $vault_cache
 notify-send -i lock -e "Vault cache refreshed"
