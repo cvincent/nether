4d3eceb4e07971dbf5a019b7f365d03c5a05b8bb
Chris Vincent <c.j.vincent@protonmail.com>
2025-01-12 21:52:36 -0600

    Sync every 10 minutes

--- apps/bitwarden/hm.nix
+++ apps/bitwarden/hm.nix
@@ -5,14 +5,19 @@
   ...
 }:
 
+let
+  bitwarden-cache-vault = (
+    pkgs.writeShellScriptBin "bitwarden-cache-vault" (builtins.readFile ./bitwarden-cache-vault.bash)
+  );
+in
 {
   home.packages = [
     pkgs.bitwarden-cli
+    bitwarden-cache-vault
     (pkgs.writeShellScriptBin "bitwarden-ensure-session" (
       builtins.readFile ./bitwarden-ensure-session.bash
     ))
     (pkgs.writeShellScriptBin "bitwarden-qutebrowser" (builtins.readFile ./bitwarden-qutebrowser.bash))
-    (pkgs.writeShellScriptBin "bitwarden-cache-vault" (builtins.readFile ./bitwarden-cache-vault.bash))
     (pkgs.writeShellScriptBin "bitwarden-search-vault" (
       builtins.readFile ./bitwarden-search-vault.bash
     ))
@@ -35,4 +40,18 @@
   };
 
   home.file."./.local/share/bitwarden/templates".source = utils.directSymlink "apps/bitwarden/templates";
+
+  systemd.user.services.sync-bitwarden = {
+    Unit.PartOf = [ "graphical.target" ];
+    Unit.After = [ "graphical.target" ];
+    Install.WantedBy = [ "graphical.target" ];
+    Service.ExecStart = "${bitwarden-cache-vault}/bin/bitwarden-cache-vault";
+    Service.Type = "oneshot";
+  };
+
+  systemd.user.timers.sync-bitwarden = {
+    Install.WantedBy = [ "timers.target" ];
+    Timer.OnCalendar = "*:00,10,20,30,40,50:00";
+    Timer.AccuracySec = "1s";
+  };
 }
