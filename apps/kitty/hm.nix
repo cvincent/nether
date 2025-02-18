b28a115b294ec17e44c9e33640712d6db63de923
Chris Vincent <c.j.vincent@protonmail.com>
2025-02-18 10:52:16 -0600

    We like the cursor trail; format

--- apps/kitty/hm.nix
+++ apps/kitty/hm.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, utils, ... }:
+{ pkgs, utils, ... }:
 
 {
   home.packages = [ pkgs.kitty ];
---
+++ apps/nvim/configs/lua/plugins/obsidian-nvim.lua
@@ -0,0 +1,74 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = false,
+  ft = "markdown",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  workspaces = {
+    name = "second-brain",
+    path = "/backup/second-brain",
+  },
+
+  completion = {
+    nvim_cmp = true,
+    min_chars = 2,
+  },
+
+  picker = {
+    name = "telescope.nvim",
+  },
+
+  mappings = {
+    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+    -- Toggle check-boxes.
+    ["<leader>td"] = {
+      action = function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      opts = { buffer = true },
+    },
+  },
+
+  -- Optional, customize how note IDs are generated given an optional title.
+  ---@param title string|?
+  ---@return string
+  note_id_func = function(title)
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return tostring(os.time()) .. "-" .. suffix
+  end,
+
+  wiki_link_func = "prepend_note_id",
+
+  -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+  -- URL it will be ignored but you can customize this behavior here.
+  ---@param url string
+  follow_url_func = function(url)
+    -- Open the URL in the default web browser.
+    vim.fn.jobstart({ "xdg-open", url })
+  end,
+
+  -- Optional, by default when you use `:ObsidianFollowLink` on a link to an image
+  -- file it will be ignored but you can customize this behavior here.
+  ---@param img string
+  follow_img_func = function(img)
+    vim.fn.jobstart({ "xdg-open", img })
+  end,
+}
