82c8c767e9eeca08954df9654e4e55d0c7bd8d5e
Chris Vincent <c.j.vincent@protonmail.com>
2025-02-21 14:07:50 -0600

    Newer nvim-px-to-rem is borked for me

--- apps/nvim/configs/lua/plugins/misc.lua
+++ apps/nvim/configs/lua/plugins/misc.lua
@@ -6,7 +6,7 @@ return {
   "PeterRincker/vim-searchlight",
 
   -- Easy px to rem conversion
-  { "jsongerber/nvim-px-to-rem", config = true },
+  { "jsongerber/nvim-px-to-rem", config = true, commit = "92c812bbc2f2ac2869c02d816c5e7c9843d9e06a" },
 
   -- Unobtrusive notifications
   "j-hui/fidget.nvim",
