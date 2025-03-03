2ed4d277aa6bab40ab3cbb765b3bab374bf60f99
Chris Vincent <c.j.vincent@protonmail.com>
2025-03-03 09:54:00 -0600

    Compile Elixir with the correct OTP

--- work/work-1/flake.nix
+++ work/work-1/flake.nix
@@ -3,13 +3,9 @@
 
   # Find package versions at nixhub.io
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     # Erlang 26.2.1
     our-erlang.url = "github:nixos/nixpkgs/f8e2ebd66d097614d51a56a755450d4ae1632df1";
-    # Elixir 1.17.2
-    our-elixir.url = "github:nixos/nixpkgs/5ed627539ac84809c78b2dd6d26a5cebeb5ae269";
-    # ElixirLS 0.25.0
-    our-elixir-ls.url = "github:nixos/nixpkgs/7cc0bff31a3a705d3ac4fdceb030a17239412210";
     # NodeJS 18.14.1
     our-nodejs.url = "github:nixos/nixpkgs/06365ba4549654b7ce58c33365c1282800e83a9e";
     # Yarn 1.22.22
@@ -27,19 +23,25 @@
         config.allowUnfree = true;
       };
 
-      elixir = inputs.our-elixir.legacyPackages.${system}.elixir_1_17;
       beamPkg = pkgs.beam.packagesWith inputs.our-erlang.legacyPackages.${system}.erlang_26;
-      our-elixir-ls = inputs.our-elixir-ls.legacyPackages.${system}.elixir-ls.override {
-        inherit elixir;
-        mixRelease = beamPkg.mixRelease.override { inherit elixir; };
-      };
 
+      our-elixir = beamPkg.elixir.overrideAttrs (old: {
+        name = "elixir-1.17.2";
+        src = pkgs.fetchFromGitHub {
+          rev = "v1.17.2";
+          sha256 = "sha256-8rb2f4CvJzio3QgoxvCv1iz8HooXze0tWUJ4Sc13dxg=";
+          owner = "elixir-lang";
+          repo = "elixir";
+        };
+      });
+
+      our-elixir-ls = pkgs.beam26Packages.elixir-ls;
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           inputs.our-erlang.legacyPackages.${system}.erlang_26
-          inputs.our-elixir.legacyPackages.${system}.elixir_1_17
+          our-elixir
           our-elixir-ls
           inputs.our-nodejs.legacyPackages.${system}.nodejs
           inputs.our-yarn.legacyPackages.${system}.yarn
