2b3d919ee6830baf5eb45d1d5d1c6c4781297f23
Chris Vincent <c.j.vincent@protonmail.com>
2025-03-15 19:47:33 -0500

    Format

--- apps/nvim/configs/lua/plugins/misc.lua
+++ apps/nvim/configs/lua/plugins/misc.lua
@@ -6,7 +6,11 @@ return {
   "PeterRincker/vim-searchlight",
 
   -- Easy px to rem conversion
-  { "jsongerber/nvim-px-to-rem", config = true, commit = "92c812bbc2f2ac2869c02d816c5e7c9843d9e06a" },
+  {
+    "jsongerber/nvim-px-to-rem",
+    config = true,
+    commit = "92c812bbc2f2ac2869c02d816c5e7c9843d9e06a"
+  },
 
   -- Unobtrusive notifications
   "j-hui/fidget.nvim",
