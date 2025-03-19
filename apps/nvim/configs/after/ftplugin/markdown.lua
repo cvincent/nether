de819a4731c2fea79d3f210c4cf08f190bdffe30
Chris Vincent <c.j.vincent@protonmail.com>
2025-03-19 09:54:32 -0500

    We only want these in Markdown

--- apps/nvim/configs/after/ftplugin/markdown.lua
+++ apps/nvim/configs/after/ftplugin/markdown.lua
@@ -2,3 +2,13 @@ vim.opt_local.conceallevel = 2
 -- The space gets in the way of easily adding checkboxes
 vim.b[0].delimitMate_expand_space = 0
 vim.api.nvim_command("set spell")
+
+-- Bullets mappings
+vim.keymap.set("i", "<cr>", "<Plug>(bullets-newline)")
+vim.keymap.set("i", "<c-cr>", "<cr>", { noremap = true })
+
+vim.keymap.set("n", "o", "<Plug>(bullets-newline)")
+vim.keymap.set("n", ">>", "<Plug>(bullets-demote)")
+vim.keymap.set("v", ">", "<Plug>(bullets-demote)")
+vim.keymap.set("n", "<<", "<Plug>(bullets-promote)")
+vim.keymap.set("v", "<", "<Plug>(bullets-promote)")
--- apps/nvim/configs/lua/plugins/bullets-vim.lua
+++ apps/nvim/configs/lua/plugins/bullets-vim.lua
@@ -5,15 +5,5 @@ return {
     vim.g.bullets_outline_levels = {}
     vim.g.bullets_max_alpha_characters = 0
     vim.g.bullets_set_mappings = 0
-
-    -- Bring in only the mappings we want
-    vim.keymap.set("i", "<cr>", "<Plug>(bullets-newline)")
-    vim.keymap.set("i", "<c-cr>", "<cr>", { noremap = true })
-
-    vim.keymap.set("n", "o", "<Plug>(bullets-newline)")
-    vim.keymap.set("n", ">>", "<Plug>(bullets-demote)")
-    vim.keymap.set("v", ">", "<Plug>(bullets-demote)")
-    vim.keymap.set("n", "<<", "<Plug>(bullets-promote)")
-    vim.keymap.set("v", "<", "<Plug>(bullets-promote)")
   end
 }
