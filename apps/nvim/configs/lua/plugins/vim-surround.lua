3fbbdc3a19392bd40c8492b01b4f4a4330f2c8c5
Chris Vincent <c.j.vincent@protonmail.com>
2025-03-26 14:32:35 -0500

    Attempt to make persistent visual surround, didn't work

--- apps/nvim/configs/lua/plugins/vim-surround.lua
+++ apps/nvim/configs/lua/plugins/vim-surround.lua
@@ -1,7 +1,19 @@
 return {
   "tpope/vim-surround",
   init = function()
-    vim.keymap.set("v", "s", "S", { remap = true })
+    vim.keymap.set("v", "s", "<Plug>VSurround")
     vim.keymap.del("s", "s")
+  end,
+
+  config = function()
+    vim.keymap.set("v", "S", function()
+      -- TODO: I would like to have a visual mode surround that reselects after,
+      -- but there is maybe no way to wait for the surround sequence to finish
+      -- before calling `gv`. I will need to dig more deeply into the actual
+      -- plugin code.
+      -- I created an issue: https://github.com/tpope/vim-surround/issues/392
+      CallPlugMapping("VSurround")
+      vim.cmd("normal gv")
+    end)
   end
 }
