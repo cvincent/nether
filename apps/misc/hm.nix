dc084895bb0f9ab40f9375cf6e247dbe3be50275
Chris Vincent <c.j.vincent@protonmail.com>
2025-04-24 07:35:21 -0500

    Bring in FreeCAD, software renderer for now

--- apps/misc/hm.nix
+++ apps/misc/hm.nix
@@ -68,6 +68,17 @@ in
     nixpkgs-unstable.ryujinx
     latest-shadps4
     xwayland-obsidian
+    (pkgs.symlinkJoin {
+      name = "FreeCAD";
+      paths = [ pkgs.freecad-wayland ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/FreeCAD \
+        --set __GLX_VENDOR_LIBRARY_NAME mesa \
+        --set __EGL_VENDOR_LIBRARY_FILENAMES ${pkgs.mesa.drivers}/share/glvnd/egl_vendor.d/50_mesa.json
+      '';
+      meta.mainProgram = "FreeCAD";
+    })
   ];
 
   programs.zathura.enable = true;
