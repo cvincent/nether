9882dafce1b26940ce434e2c241c74f6a6f1db14
Chris Vincent <c.j.vincent@protonmail.com>
2025-05-21 16:06:39 -0500

    Fix name of JIRA_AUTH_TOKEN env var

--- shell/jira-cli/hm.nix
+++ shell/jira-cli/hm.nix
@@ -2,6 +2,6 @@
 
 {
   home.packages = [ pkgs.jira-cli-go ];
-  sops.secrets.jira_api_token = {};
-  programs.fish.interactiveShellInit = "set -gx JIRA_API_TOKEN $(cat ${config.sops.secrets.jira_api_token.path})";
+  sops.secrets.jira_auth_token = { };
+  programs.fish.interactiveShellInit = "set -gx JIRA_AUTH_TOKEN $(cat ${config.sops.secrets.jira_auth_token.path})";
 }
