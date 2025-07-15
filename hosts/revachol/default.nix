b9bb830009f1d311445a07b0e0e4de296a1aa907
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-15 09:17:35 -0500

    Network settings

--- hosts/revachol/default.nix
+++ hosts/revachol/default.nix
@@ -8,6 +8,9 @@
     username = "cvincent";
     system.stateVersion = "24.11";
     home.stateVersion = "23.11";
+
+    networkmanager.enable = true;
+    tor.enable = true;
   };
 
   boot.kernelPackages = pkgs.linuxPackages_6_11;
