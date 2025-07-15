1dd8a518544f514551d5ccc61e15332581b27f2c
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-15 09:18:02 -0500

    Bring in filesystems

--- hosts/revachol/default.nix
+++ hosts/revachol/default.nix
@@ -16,4 +16,20 @@
   boot.kernelPackages = pkgs.linuxPackages_6_11;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  fileSystems."/steam" = {
+    device = "/dev/disk/by-uuid/2749d5f5-c6c6-4a62-abe2-8ebcfb0bc68a";
+    fsType = "ext4";
+    options = [ "nofail" ];
+  };
+
+  fileSystems."/backup" = {
+    device = "192.168.1.128:/storage/smb";
+    fsType = "nfs";
+    options = [
+      "nfsvers=4.2"
+      "noatime"
+      "nofail"
+    ];
+  };
 }
