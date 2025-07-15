485e9fe13b176261a2f7c9cd292faa7fd6332512
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-15 11:32:07 -0500

    Enable nix-index; temporary opt for fish

--- flake-modules/nix.nix
+++ flake-modules/nix.nix
@@ -1,18 +1,34 @@
 { name }:
-{ ... }:
+{ lib, ... }:
 {
   flake.nixosModules."${name}" = {
-    nix = {
-      settings.experimental-features = [
-        "nix-command"
-        "flakes"
-        "pipe-operator"
-      ];
+    options = {
+      # TODO: Move this to the fish module when we get there
+      nether.shells.fish.enable = lib.mkEnableOption "fish shell";
     };
 
-    optimise = {
-      automatic = true;
-      dates = [ "03:45" ];
+    config = {
+      nix = {
+        settings.experimental-features = [
+          "nix-command"
+          "flakes"
+          "pipe-operator"
+        ];
+
+        optimise = {
+          automatic = true;
+          dates = [ "03:45" ];
+        };
+      };
     };
   };
+
+  flake.homeModules."${name}" =
+    { osConfig, ... }:
+    {
+      programs.nix-index = {
+        enable = true;
+        enableFishIntegration = osConfig.nether.shells.fish.enable;
+      };
+    };
 }
