1534318cc3084523354da30db7c306e08fa66180
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-15 21:04:08 -0500

    Note

--- apps/browsers/hm.nix
+++ apps/browsers/hm.nix
@@ -1,6 +1,10 @@
 { pkgs, browser-pkgs, ... }:
 
 {
+  # Add this line to nixosModule
+  # Silences warnings on Chromium boot, and useful for checking battery levels
+  # from CLI
+  # services.upower.enable = true;
   home.packages = with pkgs; [
     (browser-pkgs.chromium.override {
       commandLineArgs = [
