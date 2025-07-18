f178e08ab916317b4e1a02214d8742ff6b811925
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-17 21:40:54 -0500

    Alright, all the GNOME shit is done

--- flake-modules/graphical-env.nix
+++ flake-modules/graphical-env.nix
@@ -6,8 +6,12 @@
   ...
 }:
 {
-  flake.nixosModules."${name}" =
+  flake.nixosModules."${name}" = moduleWithSystem (
+    { pkgs }:
     { config, ... }:
+    let
+      inherit (config.nether) graphicalEnv;
+    in
     {
       options = {
         nether.graphicalEnv = {
@@ -31,33 +35,62 @@
 
           extra = {
             gnomeKeyring.enable = helpers.boolOpt true "GNOME Keyring - some apps need this to store secrets";
-            libnotify.enable = helpers.boolOpt true "libnotify - utilities for system notifications";
+
+            gnomePolkit =
+              helpers.pkgOpt pkgs.polkit_gnome true
+                "GNOME Polkit - some apps need this to authenticate the user";
+
+            libnotify = helpers.pkgOpt pkgs.libnotify true "libnotify - utilities for system notifications";
           };
         };
       };
 
       config = lib.mkIf config.nether.graphicalEnv.enable {
+        environment.systemPackages = lib.optional graphicalEnv.extra.gnomePolkit.enable graphicalEnv.extra.gnomePolkit.package;
+
         services.xserver = {
           enable = true;
           displayManager.gdm.enable = config.nether.graphicalEnv.displayManager == "gdm";
         };
 
+        services.gnome.gnome-keyring.enable = config.nether.graphicalEnv.extra.gnomeKeyring.enable;
+
+        # Uses login password to unlock the GNOME Keyring
         security.pam.services.login.enableGnomeKeyring =
           config.nether.graphicalEnv.extra.gnomeKeyring.enable;
-        services.gnome.gnome-keyring.enable = config.nether.graphicalEnv.extra.gnomeKeyring.enable;
-      };
-    };
 
-  flake.homeModules."${name}" = moduleWithSystem (
-    { pkgs }:
+        security.polkit = {
+          enable = graphicalEnv.extra.gnomePolkit.enable;
+          adminIdentities = [ "unix-user:${config.nether.username}" ];
+        };
+
+        systemd = lib.mkIf graphicalEnv.extra.gnomePolkit.enable {
+          user.services.polkit-gnome-authentication-agent-1 = {
+            description = "polkit-gnome-authentication-agent-1";
+            wantedBy = [ "graphical-session.target" ];
+            wants = [ "graphical-session.target" ];
+            after = [ "graphical-session.target" ];
+            serviceConfig = {
+              Type = "simple";
+              ExecStart = "${graphicalEnv.extra.gnomePolkit.package}/libexec/polkit-gnome-authentication-agent-1";
+              Restart = "on-failure";
+              RestartSec = 1;
+              TimeoutStopSec = 10;
+            };
+          };
+        };
+      };
+    }
+  );
+
+  flake.homeModules."${name}" =
     { osConfig, ... }:
     let
       inherit (osConfig.nether) graphicalEnv;
     in
     {
       config = lib.mkIf graphicalEnv.enable {
-        home.packages = lib.optional graphicalEnv.extra.libnotify.enable pkgs.libnotify;
+        home.packages = lib.optional graphicalEnv.extra.libnotify.enable graphicalEnv.extra.libnotify.package;
       };
-    }
-  );
+    };
 }
