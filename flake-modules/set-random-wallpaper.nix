512735c0d6cd3406392ad2698ff33e5f416e83c1
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-18 14:20:08 -0500

    set-random-wallpaper

---
+++ flake-modules/set-random-wallpaper.nix
@@ -0,0 +1,46 @@
+{ name }:
+{ lib, moduleWithSystem, ... }:
+{
+  flake.homeModules."${name}" = moduleWithSystem (
+    { pkgs }:
+    { config, osConfig, ... }:
+    let
+      inherit (config.home) homeDirectory;
+      inherit (osConfig.nether.graphicalEnv) wallpapers;
+
+      set-random-wallpaper = (
+        pkgs.writeShellScriptBin "set-random-wallpaper" ''
+          PATH=$PATH:/run/current-system/sw/bin
+
+          top_wallpaper=$(find ${homeDirectory}/dotfiles/wallpapers/nord-* -type f | sort -R | head -n1)
+          ${wallpapers.swww.package}/bin/swww img -o DP-2 $top_wallpaper
+
+          landscape_wallpaper=$(find ${homeDirectory}/dotfiles/wallpapers/landscapes/nord-*-m0\.* -type f | sort -R | head -n1)
+          landscape_right=$''\{landscape_wallpaper/m0\./m1\.}
+          ${wallpapers.swww.package}/bin/swww img -o DP-3 $landscape_wallpaper
+          ${wallpapers.swww.package}/bin/swww img -o DP-1 $landscape_right
+        ''
+      );
+    in
+    {
+      # TODO: Consider making this agnostic about our wallpaper switcher
+      config = lib.mkIf (wallpapers.which == "swww") {
+        home.packages = [ set-random-wallpaper ];
+
+        systemd.user.services.set-random-wallpaper = {
+          Unit.PartOf = [ "graphical.target" ];
+          Unit.After = [ "graphical.target" ];
+          Install.WantedBy = [ "graphical.target" ];
+          Service.ExecStart = "${set-random-wallpaper}/bin/set-random-wallpaper";
+          Service.Type = "oneshot";
+        };
+
+        systemd.user.timers.set-random-wallpaper = {
+          Install.WantedBy = [ "timers.target" ];
+          Timer.OnCalendar = "*:00,20,40:00";
+          Timer.AccuracySec = "1s";
+        };
+      };
+    }
+  );
+}
