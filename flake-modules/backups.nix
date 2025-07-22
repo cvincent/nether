e34c60d3aeac7cfbcc79c58919a4ae9f95982db5
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-21 21:31:56 -0500

    Forgot option

--- flake-modules/backups.nix
+++ flake-modules/backups.nix
@@ -13,6 +13,29 @@
       options = {
         nether.backups = {
           enable = lib.mkEnableOption "System for automatically backing up select files to the network drive, and restoring them on activation";
+
+          mount = {
+            # TODO: Ensure references to /backup throughout the config refer to
+            # this option
+            path = lib.mkOption {
+              type = lib.types.str;
+              default = "/backup";
+            };
+
+            device = lib.mkOption {
+              type = lib.types.str;
+              default = "192.168.1.128:/storage/smb";
+            };
+
+            options = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [
+                "nfsvers=4.2"
+                "noatime"
+                "nofail"
+              ];
+            };
+          };
         };
       };
 
