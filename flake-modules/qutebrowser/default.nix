95b78a6aac1f1b4b84d7fde51c66f0388f8bd0b0
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-24 11:12:21 -0500

    Update yt-dlp destination path

--- flake-modules/qutebrowser/default.nix
+++ flake-modules/qutebrowser/default.nix
@@ -150,11 +150,11 @@
               "<space>fv" =
                 "hint links spawn mpv --force-window=immediate --no-terminal --keep-open=yes --http-proxy='https://192.168.1.114:8888' --ytdl-raw-options='proxy=http://192.168.1.114:8888' {hint-url}";
               "<space>fV" =
-                "hint links spawn yt-dlp -P home:~/youtube/ -P temp:~/youtube/.tmp/ -o '%(duration)s - %(title)s.%(ext)s' --embed-chapters --proxy http://192.168.1.114:8888 {hint-url}";
+                "hint links spawn yt-dlp -P home:~/videos/youtube/ -P temp:~/videos/youtube/.tmp/ -o '%(duration)s - %(title)s.%(ext)s' --embed-chapters --proxy http://192.168.1.114:8888 {hint-url}";
               "yv" =
                 "spawn mpv --force-window=immediate --no-terminal --keep-open=yes --http-proxy='https://192.168.1.114:8888' {url}";
               "yV" =
-                "spawn yt-dlp -P home:~/youtube/ -P temp:~/youtube/.tmp/ -o '%(duration)s - %(title)s.%(ext)s' --embed-chapters --proxy http://192.168.1.114:8888 {url}";
+                "spawn yt-dlp -P home:~/videos/youtube/ -P temp:~/videos/youtube/.tmp/ -o '%(duration)s - %(title)s.%(ext)s' --embed-chapters --proxy http://192.168.1.114:8888 {url}";
 
               # Downloads
               "<space>dc" = "download-clear";
