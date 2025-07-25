ece9b954849f145476988007ac1eeae46778eacb
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-25 14:20:48 -0500

    Easy access to default terminal package and path

--- flake-modules/terminals.nix
+++ flake-modules/terminals.nix
@@ -1,22 +1,44 @@
 { name }:
-{ lib, ... }:
+{
+  lib,
+  moduleWithSystem,
+  helpers,
+  ...
+}:
 {
   imports = [ (import ./kitty { name = "kitty"; }) ];
 
-  flake.nixosModules."${name}" = {
-    options = {
-      nether.terminals = {
-        kitty.enable = lib.mkEnableOption "Kitty terminal emulator";
+  flake.nixosModules."${name}" = moduleWithSystem (
+    { pkgs }:
+    { config, ... }:
+    {
+      options = {
+        nether.terminals = {
+          kitty = helpers.pkgOpt pkgs.kitty (
+            config.nether.terminals.default == "kitty"
+          ) "Kitty terminal emulator";
 
-        # TODO: Propagate this where we currently reference kitty
-        default = lib.mkOption {
-          type = lib.types.enum [
-            null
-            "kitty"
-          ];
-          default = null;
+          # TODO: Propagate this where we currently reference kitty
+          default = lib.mkOption {
+            type = lib.types.enum [
+              null
+              "kitty"
+            ];
+            default = null;
+          };
+
+          defaultPackage = lib.mkOption { type = lib.types.package; };
+          defaultPath = lib.mkOption { type = lib.types.str; };
         };
       };
-    };
-  };
+
+      config.nether.terminals.defaultPackage = lib.mkIf config.nether.terminals.default (
+        lib.mkForce config.nether.terminals."${config.nether.terminals.default}".package
+      );
+
+      config.nether.terminals.defaultPath = lib.mkIf config.nether.terminals.default (
+        lib.mkForce "${config.nether.terminals.defaultPackage}/bin/${config.nether.terminals.default}"
+      );
+    }
+  );
 }
