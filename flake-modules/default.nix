a592f53a4059240fe57366516dadd7986c20a69a
Chris Vincent <c.j.vincent@protonmail.com>
2025-07-28 11:12:44 -0500

    This directory no longer has its own modules

--- flake-modules/default.nix
+++ flake-modules/default.nix
@@ -1,15 +1,6 @@
-{ lib, ... }:
-let
-  modules =
-    ./.
-    |> builtins.readDir
-    |> (lib.attrsets.filterAttrs (_k: v: v == "regular"))
-    |> lib.attrsets.attrNames
-    |> (lib.lists.remove "default.nix")
-    |> (lib.lists.map (name: lib.strings.removeSuffix ".nix" name));
-in
+{ ... }:
 {
-  imports = (lib.map (mod: (import ./${mod}.nix { name = mod; })) modules) ++ [
+  imports = [
     ./features
     ./scripts
     ./software
