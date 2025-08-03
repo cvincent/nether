c143b0dda9a21a2a49e5bc3342a5edf54cbf69f5
Chris Vincent <c.j.vincent@protonmail.com>
2025-08-03 15:26:42 -0500

    Be more straightforward

--- flake-modules/features/shells.nix
+++ flake-modules/features/shells.nix
@@ -20,39 +20,30 @@ mkFeature name (
       };
     };
 
-    extra =
-      {
-        description = "Various useful shell utilities";
-        eza.config.enableFishIntegration = shells.fish.enable;
-        starship.config.enableFishIntegration = shells.fish.enable;
-        zoxide.config.enableFishIntegration = shells.fish.enable;
-      }
-      |> lib.recursiveUpdate (
-        [
-          "bat"
-          "btop"
-          "direnv"
-          "fastfetch"
-          "fd"
-          "fzf"
-          "gh"
-          "jq"
-          "ripgrep"
-          "d2"
-          "dua"
-          "duf"
-          "dust"
-          "fx"
-          "unzip"
-          "magic-wormhole"
-          "zf"
-        ]
-        |> map (package: {
-          name = package;
-          value = { };
-        })
-        |> lib.listToAttrs
-      );
+    extra = {
+      description = "Various useful shell utilities";
+
+      bat = { };
+      btop = { };
+      d2 = { };
+      direnv = { };
+      dua = { };
+      duf = { };
+      dust = { };
+      eza.config.enableFishIntegration = shells.fish.enable;
+      fastfetch = { };
+      fd = { };
+      fx = { };
+      fzf = { };
+      gh = { };
+      jq = { };
+      magic-wormhole = { };
+      ripgrep = { };
+      starship.config.enableFishIntegration = shells.fish.enable;
+      unzip = { };
+      zf = { };
+      zoxide.config.enableFishIntegration = shells.fish.enable;
+    };
 
     # TODO: Figure out why lib.mkForce is needed here, it's very strange
     nixos.nether.shells = lib.mkForce { aliases.mkdir = "mkdir -p"; };
