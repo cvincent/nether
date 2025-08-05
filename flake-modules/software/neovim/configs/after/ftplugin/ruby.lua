1f8f9bf3d1e0c333cd0fa30a0a35db0463068963
Chris Vincent <c.j.vincent@protonmail.com>
2025-08-05 09:32:15 -0500

    Improved matchit support for Ruby

---
+++ flake-modules/software/neovim/configs/after/ftplugin/ruby.lua
@@ -0,0 +1,5 @@
+-- Improved Ruby support for the built-in matchit plugin (%)
+vim.b.match_ignorecase = 0
+vim.b.match_words =
+    [[\%(\%(\%(^\|[;=]\)\s*\)\@<=\%(class\|module\|while\|begin\|until\|for\|if\|unless\|def\|case\)\|\<do\)\>:]] ..
+    [[\<\%(else\|elsif\|ensure\|rescue\|when\)\>:\%(^\|[^.]\)\@<=\<end\>]]
