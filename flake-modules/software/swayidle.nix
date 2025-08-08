a65d35c3af3db7eac67500ebcd5906e74aa549c6
Chris Vincent <c.j.vincent@protonmail.com>
2025-08-08 13:46:19 -0500

    mkSoftware for swayidle

---
+++ flake-modules/software/swayidle.nix
@@ -0,0 +1,22 @@
+{ name, mkSoftware, ... }:
+mkSoftware name (
+  {
+    hmOptions,
+    swayidle,
+    lib,
+    ...
+  }:
+  {
+    options = lib.getAttrs [ "events" "timeouts" ] hmOptions.services.swayidle;
+
+    hm.services.swayidle = {
+      inherit (swayidle)
+        enable
+        package
+        events
+        timeouts
+        ;
+      systemdTarget = "graphical.target";
+    };
+  }
+)
