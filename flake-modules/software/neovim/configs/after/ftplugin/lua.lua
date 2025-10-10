37a6ff8c4e86ccdd82915aeae80847109211c9b1
Chris Vincent <c.j.vincent@protonmail.com>
2025-10-10 13:03:07 -0500

    Use the 0.11 documentation mappings

--- flake-modules/software/neovim/configs/after/ftplugin/lua.lua
+++ flake-modules/software/neovim/configs/after/ftplugin/lua.lua
@@ -1,4 +1,4 @@
 -- Execute the current line of Lua
-vim.keymap.set("n", "<leader>x", ":.lua<cr>")
+vim.keymap.set("n", "g==", ":.lua<cr>")
 -- Execute the selected lines of Lua
-vim.keymap.set("v", "<leader>x", ":lua<cr>")
+vim.keymap.set("v", "g=", ":lua<cr>")
