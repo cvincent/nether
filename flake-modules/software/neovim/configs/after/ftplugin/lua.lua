8df5919d7a4bd60a8c60d7c5b1b41a0edfb4dba5
Chris Vincent <c.j.vincent@protonmail.com>
2025-10-12 18:53:17 -0500

    Note

--- flake-modules/software/neovim/configs/after/ftplugin/lua.lua
+++ flake-modules/software/neovim/configs/after/ftplugin/lua.lua
@@ -1,4 +1,5 @@
 -- Execute the current line of Lua
 vim.keymap.set("n", "g==", ":.lua<cr>")
 -- Execute the selected lines of Lua
+-- TODO: Make this work in normal mode with motions
 vim.keymap.set("v", "g=", ":lua<cr>")
