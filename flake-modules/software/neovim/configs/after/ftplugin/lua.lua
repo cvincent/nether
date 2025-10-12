2266337d7ca38c195ce1cc52a5ef3a35fa9ff396
Chris Vincent <c.j.vincent@protonmail.com>
2025-10-12 18:54:51 -0500

    <c-e> adds trailing comma inside table definitions

--- flake-modules/software/neovim/configs/after/ftplugin/lua.lua
+++ flake-modules/software/neovim/configs/after/ftplugin/lua.lua
@@ -3,3 +3,14 @@ vim.keymap.set("n", "g==", ":.lua<cr>")
 -- Execute the selected lines of Lua
 -- TODO: Make this work in normal mode with motions
 vim.keymap.set("v", "g=", ":lua<cr>")
+
+local ts_utils = require("ts_utils")
+
+vim.keymap.set("i", "<c-e>", function()
+  if ts_utils.find_node_ancestor({ "table_constructor" }, vim.treesitter.get_node()) then
+    local keys = vim.api.nvim_replace_termcodes("<space>=<space>,<left>", true, true, true)
+    vim.fn.feedkeys(keys)
+  else
+    vim.fn.feedkeys(" = ")
+  end
+end)
