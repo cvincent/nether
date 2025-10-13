210a783bc9229973c3b4c3301d5f64a76b2e0240
Chris Vincent <c.j.vincent@protonmail.com>
2025-10-13 08:52:30 -0500

    Initial nvim-treesitter-textobjects setup

--- flake-modules/software/neovim/configs/lua/plugins/nvim-treesitter.lua
+++ flake-modules/software/neovim/configs/lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,9 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+
     config = function()
       local opts = {
         auto_install = true,
@@ -27,6 +30,51 @@ return {
           enable = true,
           -- Treesitter indent doesn't handle list continuations well
           disable = { "markdown" }
+        },
+
+        textobjects = {
+          select = {
+            enable = true,
+
+            keymaps = {
+              ["ia"] = "@parameter.inner",
+              ["aa"] = "@parameter.outer",
+              ["im"] = "@function.inner",
+              ["am"] = "@function.outer",
+            },
+
+            selection_modes = {
+              ["@function.inner"] = "V",
+              ["@function.outer"] = "V",
+            },
+          },
+
+          swap = {
+            enable = true,
+
+            swap_next = {
+              ["<leader>xa"] = "@parameter.inner", -- swap parameters/argument with next
+              ["<leader>xm"] = "@function.outer",  -- swap function with next
+            },
+
+            swap_previous = {
+              ["<leader>xA"] = "@parameter.inner", -- swap parameters/argument with prev
+              ["<leader>xM"] = "@function.outer",  -- swap function with previous
+            },
+          },
+
+          move = {
+            enable = true,
+            set_jumps = true,
+
+            goto_next_start = {
+              ["]a"] = "@parameter.outer",
+            },
+
+            goto_previous_start = {
+              ["[a"] = "@parameter.outer",
+            },
+          },
         },
       }
 
