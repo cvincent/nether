58a179e0303e9ca149a9b140043b0fde1f2d70c1
Chris Vincent <c.j.vincent@protonmail.com>
2025-12-26 14:51:52 -0600

    Use our custom action

--- flake-modules/software/neovim/configs/lua/plugins/telescope-nvim.lua
+++ flake-modules/software/neovim/configs/lua/plugins/telescope-nvim.lua
@@ -56,21 +56,7 @@ return {
         buffers = {
           mappings = {
             i = {
-              ["<cr>"] = function(prompt_bufnr)
-                -- Open existing window for buffer if there is one, otherwise
-                -- perform the default behavior (open buffer in current window)
-                local action_state = require("telescope.actions.state")
-                local actions = require("telescope.actions")
-                local entry = action_state.get_selected_entry()
-
-                local winid = vim.fn.win_findbuf(entry.bufnr)
-
-                if #winid > 0 then
-                  vim.fn.win_gotoid(winid[1])
-                else
-                  actions.select_default(prompt_bufnr)
-                end
-              end,
+              ["<cr>"] = require("custom_telescopes.actions").find_existing_window,
             },
           },
         },
