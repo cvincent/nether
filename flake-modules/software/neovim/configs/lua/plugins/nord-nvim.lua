34192a49a4338f8da51b69356cf576db203e72e1
Chris Vincent <c.j.vincent@protonmail.com>
2025-12-26 14:48:20 -0600

    More tolerable diff colors

--- flake-modules/software/neovim/configs/lua/plugins/nord-nvim.lua
+++ flake-modules/software/neovim/configs/lua/plugins/nord-nvim.lua
@@ -4,6 +4,7 @@ return {
     local colors = require("nord.colors")
 
     vim.g.nord_disable_background = true
+    vim.g.nord_uniform_diff_background = true
     vim.g.nord_italic = false
     vim.g.nord_bold = false
 
