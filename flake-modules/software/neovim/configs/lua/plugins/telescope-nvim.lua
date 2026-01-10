8cbd9882b9d64628cd5f904b85199642eda8d7f3
Chris Vincent <c.j.vincent@protonmail.com>
2026-01-10 13:38:55 -0600

    Lots of telecope-nvim stuff...
    - Using telescope-live-grep-args so we can finally use the full power of
      Ripgrep within NeoVim.
    - <c-s-q> appends current results to quickfix list, as opposed to <c-q>
      which replaces the quickfix list.
    - Setting `opts` as a function so we don't have to repeatedly `require`.
    - Move the whole thing down one line so our status bar doesn't look like
      it's part of the Telescope window.
    - <leader>fk for keymaps, finally mapped it.
    - Possibly more? This diff is a bit of a mess. But I think that covers
      it.

--- flake-modules/software/neovim/configs/lua/plugins/telescope-nvim.lua
+++ flake-modules/software/neovim/configs/lua/plugins/telescope-nvim.lua
@@ -5,7 +5,7 @@ return {
 
     dependencies = {
       "nvim-lua/plenary.nvim",
-      -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-telescope/telescope-live-grep-args.nvim",
       "natecraddock/telescope-zf-native.nvim",
       "nvim-telescope/telescope-symbols.nvim",
       {
@@ -14,102 +14,128 @@ return {
       },
     },
 
-    opts = {
-      defaults = vim.tbl_extend(
-        "force",
-        require("telescope.themes").get_ivy(),
-        {
-          mappings = {
-            i = {
-              ["<m-bs>"] = { "<c-s-w>", type = "command" },
-              ["<esc>"] = "close",
-              ["<c-down>"] = "cycle_history_next",
-              ["<c-up>"] = "cycle_history_prev",
+    opts = function()
+      require("telescope")
+      local telescope_actions = require("telescope.actions")
+      local lga_actions = require("telescope-live-grep-args.actions")
 
-              ["<c-q>"] = function(prompt_bufnr)
-                local actions = require("telescope.actions")
-                actions.smart_send_to_qflist(prompt_bufnr)
-                actions.open_qflist(prompt_bufnr)
-              end,
-            }
-          },
-          winblend = 15,
-          prompt_prefix = "❯ ",
-          selection_caret = "→ ",
-          dynamic_preview_title = true,
-          results_title = false,
-          cache_picker = { num_pickers = -1 },
-          layout_strategy = "horizontal",
-          layout_config = {
-            anchor = "S",
-            anchor_padding = 0,
-            prompt_position = "top",
-            height = 0.4,
-            width = 9999,
-            mirror = true,
-            preview_width = 0.5,
-          },
-          -- TODO: Make the theme look like our git fugitive window in progress
-        }),
+      return {
+        defaults = vim.tbl_extend(
+          "force",
+          require("telescope.themes").get_ivy(),
+          {
+            mappings = {
+              i = {
+                ["<m-bs>"] = { "<c-s-w>", type = "command" },
+                ["<esc>"] = "close",
+                ["<c-down>"] = "cycle_history_next",
+                ["<c-up>"] = "cycle_history_prev",
 
-      pickers = {
-        buffers = {
-          mappings = {
-            i = {
-              ["<cr>"] = require("custom_telescopes.actions").find_existing_window,
+                ["<c-q>"] = function(prompt_bufnr)
+                  local actions = telescope_actions
+                  actions.smart_send_to_qflist(prompt_bufnr)
+                  actions.open_qflist(prompt_bufnr)
+                end,
+
+                ["<c-s-q>"] = function(prompt_bufnr)
+                  local actions = telescope_actions
+                  actions.smart_add_to_qflist(prompt_bufnr)
+                  actions.open_qflist(prompt_bufnr)
+                end,
+
+                ["<c-space>"] = telescope_actions.to_fuzzy_refine,
+              }
+            },
+            winblend = 15,
+            prompt_prefix = "❯ ",
+            selection_caret = "→ ",
+            dynamic_preview_title = true,
+            results_title = false,
+            cache_picker = { num_pickers = -1 },
+            layout_strategy = "horizontal",
+            layout_config = {
+              anchor = "S",
+              anchor_padding = -1,
+              prompt_position = "top",
+              height = 0.4,
+              width = 9999,
+              mirror = true,
+              preview_width = 0.5,
+            },
+            -- TODO: Make the theme look like our git fugitive window in progress
+          }),
+
+        pickers = {
+          buffers = {
+            mappings = {
+              i = {
+                ["<cr>"] = require("custom_telescopes.actions").find_existing_window,
+              },
+            },
+          },
+
+          git_branches = {
+            mappings = {
+              i = { ["<cr>"] = telescope_actions.git_switch_branch }
             },
           },
         },
 
-        git_branches = {
-          mappings = {
-            i = { ["<cr>"] = require("telescope.actions").git_switch_branch }
+        extensions = {
+          tailiscope = {
+            register = "c",
+          },
+
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ["<c-k>"] = lga_actions.quote_prompt(),
+                ["<c-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              },
+            },
           },
         },
-      },
-
-      extensions = {
-        tailiscope = {
-          register = "c",
-        },
-      },
-    },
+      }
+    end,
 
     init = function()
-      -- require("telescope").load_extension("fzf")
-      require("telescope").load_extension("zf-native")
+      local telescope = require("telescope")
+      local telescope_actions = require("telescope.actions")
+      local telescope_builtin = require("telescope.builtin")
 
-      vim.keymap.set("n", "<leader>fs", require("telescope.builtin").lsp_document_symbols)
-      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
-      vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
-      vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
-      vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
-      vim.keymap.set("n", "<leader>fe", require("telescope.builtin").symbols)
-      vim.keymap.set("n", "<leader>fT", require("telescope.builtin").builtin)
-      vim.keymap.set("n", "<leader>ft", require("custom_telescopes.pickers").tmux_files)
+      local custom_telescopes_pickers = require("custom_telescopes.pickers")
+      local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
 
-      vim.keymap.set(
-        "n", "<leader>fG",
-        function()
-          require("telescope.builtin").live_grep({ default_text = vim.fn.expand("<cword>") })
-        end
-      )
+      vim.keymap.set("n", "<leader>fs", telescope_builtin.lsp_document_symbols)
+      vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files)
+      vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers)
+      vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags)
+      vim.keymap.set("n", "<leader>fe", telescope_builtin.symbols)
+      vim.keymap.set("n", "<leader>fk", telescope_builtin.keymaps)
+      vim.keymap.set("n", "<leader>fT", telescope_builtin.builtin)
+      vim.keymap.set("n", "<leader>ft", custom_telescopes_pickers.tmux_files)
 
-      vim.keymap.set("n", "<leader>gc", require("telescope.builtin").git_branches)
+      vim.keymap.set("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args)
+      vim.keymap.set("n", "<leader>fG", live_grep_args_shortcuts.grep_word_under_cursor)
+
+      vim.keymap.set("n", "<leader>gc", telescope_builtin.git_branches)
 
       vim.keymap.set(
         "n", "<leader>gm",
         function()
-          require("telescope.builtin").git_branches({
+          telescope_builtin.git_branches({
             attach_mappings = function(_, map)
-              map("i", "<cr>", require("telescope.actions").git_merge_branch)
+              map("i", "<cr>", telescope_actions.git_merge_branch)
               return true
             end
           })
         end
       )
 
-      require("telescope").load_extension("tailiscope")
+      telescope.load_extension("live_grep_args")
+      telescope.load_extension("tailiscope")
+      telescope.load_extension("zf-native")
     end,
   },
 }
