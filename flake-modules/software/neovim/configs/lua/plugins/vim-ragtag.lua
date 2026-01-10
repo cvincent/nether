c0f141f49382dbe63c8920064608797321ed92c9
Chris Vincent <c.j.vincent@protonmail.com>
2026-01-10 13:38:55 -0600

    Consolidate vim-ragtag autocommands
    - Cryptic note, _does_ that belong here?
    - Revisit ragtag, is this still how we want to handle tags? We know
      vim-surrounds can do some of the stuff. What do we specifically use
      vim-ragtag for?

--- flake-modules/software/neovim/configs/lua/plugins/vim-ragtag.lua
+++ flake-modules/software/neovim/configs/lua/plugins/vim-ragtag.lua
@@ -1,25 +1,11 @@
 return {
   "tpope/vim-ragtag",
-  -- NOTE: I should actually just call RagtagInit() in after/ftplugin, duh
   init = function()
     vim.api.nvim_create_autocmd("filetype", {
-      pattern = "elixir",
+      pattern = { "elixir", "eelixir", "heex" },
       callback = function()
         vim.cmd("call RagtagInit()")
-        vim.cmd("source ~/.config/nvim/after/indent/heex.lua")
-      end
-    })
-    vim.api.nvim_create_autocmd("filetype", {
-      pattern = "eelixir",
-      callback = function()
-        vim.cmd("call RagtagInit()")
-        vim.cmd("source ~/.config/nvim/after/indent/heex.lua")
-      end
-    })
-    vim.api.nvim_create_autocmd("filetype", {
-      pattern = "heex",
-      callback = function()
-        vim.cmd("call RagtagInit()")
+        -- TODO: Does this belong here...?
         vim.cmd("source ~/.config/nvim/after/indent/heex.lua")
       end
     })
