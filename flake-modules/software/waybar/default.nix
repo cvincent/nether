88af3b99c4455b6f45aca504655bd92c58e57bbf
Chris Vincent <c.j.vincent@protonmail.com>
2026-01-10 13:38:55 -0600

    Waybar imports direct symlinked custom CSS
    Best of both worlds in terms of generating the theme colors from Stylix
    and being able to rapidly iterate on our actual custom CSS without
    rebuilding.

--- flake-modules/software/waybar/default.nix
+++ flake-modules/software/waybar/default.nix
@@ -4,8 +4,13 @@ mkSoftware name (
     config,
     waybar,
     helpers,
+    nether,
+    hmConfig,
     ...
   }:
+  let
+    customCssPath = "waybar/custom.css";
+  in
   {
     hm = {
       programs.waybar = {
@@ -14,21 +19,21 @@ mkSoftware name (
       };
 
       xdg.configFile."waybar/config".source = helpers.directSymlink ./configs/config;
+      xdg.configFile.${customCssPath}.source = helpers.directSymlink ./configs/custom.css;
 
-      xdg.configFile."waybar/style.css".text =
-        with config.lib.stylix.colors.withHashtag;
-        ''
-          @define-color base00 ${base00}; @define-color base01 ${base01};
-          @define-color base02 ${base02}; @define-color base03 ${base03};
-          @define-color base04 ${base04}; @define-color base05 ${base05};
-          @define-color base06 ${base06}; @define-color base07 ${base07};
+      xdg.configFile."waybar/style.css".text = with config.lib.stylix.colors.withHashtag; ''
+        @define-color base00 ${base00}; @define-color base01 ${base01};
+        @define-color base02 ${base02}; @define-color base03 ${base03};
+        @define-color base04 ${base04}; @define-color base05 ${base05};
+        @define-color base06 ${base06}; @define-color base07 ${base07};
 
-          @define-color base08 ${base08}; @define-color base09 ${base09};
-          @define-color base0A ${base0A}; @define-color base0B ${base0B};
-          @define-color base0C ${base0C}; @define-color base0D ${base0D};
-          @define-color base0E ${base0E}; @define-color base0F ${base0F};
-        ''
-        + (builtins.readFile ./configs/style.css);
+        @define-color base08 ${base08}; @define-color base09 ${base09};
+        @define-color base0A ${base0A}; @define-color base0B ${base0B};
+        @define-color base0C ${base0C}; @define-color base0D ${base0D};
+        @define-color base0E ${base0E}; @define-color base0F ${base0F};
+
+        @import url("${nether.homeDirectory}/${hmConfig.xdg.configFile.${customCssPath}.target}");
+      '';
     };
   }
 )
