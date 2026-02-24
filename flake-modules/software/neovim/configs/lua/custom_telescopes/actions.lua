eab77dfc5ad1571b090f93db3e8680d28fd5b26e
Chris Vincent <c.j.vincent@protonmail.com>
2026-02-23 20:22:06 -0600

    Preserve location in jumplist in a couple places
    - Before jumping by relative lines by more than 10 (e.g. `20j`)
    - When jumping location in a focused window via our custom
      `find_existing_window` Telescope action (which is used by our Tmux
      buffer file/line mentions Telescope). Currently only when focusing an
      existing window, may not be final. We might preserve the location
      before _any_ such jump.

--- flake-modules/software/neovim/configs/lua/custom_telescopes/actions.lua
+++ flake-modules/software/neovim/configs/lua/custom_telescopes/actions.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 M.find_existing_window = function(prompt_bufnr)
-  -- Open existing window for buffer if there is one, otherwise
-  -- perform the default behavior (open buffer in current window)
+  -- Focus existing window for buffer if there is one, otherwise perform the
+  -- default behavior (open buffer in current window)
   local action_state = require("telescope.actions.state")
   local actions = require("telescope.actions")
   local entry = action_state.get_selected_entry()
@@ -11,7 +11,8 @@ M.find_existing_window = function(prompt_bufnr)
 
   if #winid > 0 then
     vim.fn.win_gotoid(winid[1])
-    vim.fn.execute(':' .. entry.lnum)
+    vim.cmd("norm m'")
+    vim.fn.execute(":" .. entry.lnum)
   else
     actions.select_default(prompt_bufnr)
   end
--- flake-modules/software/neovim/configs/lua/rc/jumplist.lua
+++ flake-modules/software/neovim/configs/lua/rc/jumplist.lua
@@ -1,6 +1,20 @@
 -- Jumplist behaves like a stack, and remembers scroll position
 vim.opt.jumpoptions = { "stack", "view" }
 
+-- Jumping by relative lines is added to the jumplist over a certain distance
+local jump_with_jumplist_threshold = function(key)
+  return function()
+    if vim.v.count > 10 then
+      vim.cmd("norm! m'" .. vim.v.count .. key)
+    else
+      vim.cmd("norm! " .. vim.v.count .. key)
+    end
+  end
+end
+
+vim.keymap.set("n", "j", jump_with_jumplist_threshold("j"))
+vim.keymap.set("n", "k", jump_with_jumplist_threshold("k"))
+
 -- Ignore jumplist on { and }
 -- vim.keymap.set("n", "{", ":<c-u>execute 'keepjumps norm! ' . v:count1 . '{'<cr>", { silent = true })
 -- vim.keymap.set("n", "}", ":<c-u>execute 'keepjumps norm! ' . v:count1 . '}'<cr>", { silent = true })
